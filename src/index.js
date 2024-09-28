0573837d74f1177954de510c3ae5f771545d94bf
alanjames00 <alanjames6282@gmail.com>
2024-09-28 14:03:40 +0530

    add auth middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { Hono } from 'hono';
+import { createMiddleware } from 'hono/factory';
 import { getCookie, getSignedCookie, setCookie, setSignedCookie, deleteCookie } from 'hono/cookie';
 import renderHomepage from './views/home';
 import renderLoginPage from './views/login';
@@ -6,13 +7,34 @@ import renderLoginPage from './views/login';
 const app = new Hono();
 
 // session authentication middleware
-const authWare = (c, next) => {
-	const session = getSignedCookie(c, 'session');
+
+const authWare = createMiddleware(async (c, next) => {
+	console.log(`[${c.req.method}] ${c.req.url}`);
+	console.log('checking for session');
+
+	const session = await getCookie(c, 'session');
+	console.log('session', session);
+
 	if (!session) {
 		return c.redirect('/login');
 	}
-	// validate the session
-};
+	const { results: serverSession } = await c.env.DB.prepare(`SELECT * FROM Sessions WHERE SessionId = ? AND UserEmail = ?`)
+		.bind(session, c.env.USER_EMAIL)
+		.all();
+	console.log('serverSession', serverSession);
+	if (serverSession.length === 0) {
+		// session not found in session store
+		return c.redirect('/login');
+	}
+	if (serverSession[0].SessionId !== session) {
+		// session not found in session store
+		return c.redirect('/login');
+	}
+
+	await next();
+});
+
+app.use('/', authWare);
 
 app.get('/', (c) => {
 	return c.render(renderHomepage());
@@ -32,8 +54,13 @@ app.post('/login', async (c) => {
 	console.log('isMatched', isMatched);
 
 	if (isMatched) {
-		const session = Math.random().toString(36).slice(2);
-		setSignedCookie(c, 'session', session);
+		const session = await crypto.randomUUID();
+
+		// insert to sessions table
+		const { result } = await c.env.DB.prepare(`UPDATE Sessions SET SessionId = ? WHERE UserEmail = ?`).bind(session, email).all();
+		console.log('result of query', result);
+
+		await setCookie(c, 'session', session, { httpOnly: true, maxAge: 60 * 10 });
 		return c.redirect('/');
 	} else {
 		return c.render(renderLoginPage({ wrongCred: true }));
