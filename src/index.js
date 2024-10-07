480f3a9e76f264fbc99d2adb9855f0367cafbbcd
alanjames00 <alanjames6282@gmail.com>
2024-10-08 01:05:17 +0530

    feat: scan and save new token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { createMiddleware } from 'hono/factory';
 import { getCookie, getSignedCookie, setCookie, setSignedCookie, deleteCookie } from 'hono/cookie';
 import renderHomepage from './views/home';
 import renderLoginPage from './views/login';
+import renderScannerPage from './views/scanner';
+import generateTotp from './utils/generateTotp';
+import { encrypt, decrypt } from './utils/storeKey';
 
 const app = new Hono();
 
@@ -67,4 +70,58 @@ app.post('/login', async (c) => {
 	}
 });
 
+app.get('/scan', async (c) => {
+	return c.render(renderScannerPage());
+});
+
+// generate a totp token before storing it
+app.post('/api/token/new', async (c) => {
+	const { algorithm, digits, issuer, label, secret, period } = await c.req.json();
+
+	const otp = await generateTotp({
+		algorithm,
+		digits,
+		period,
+		secret,
+	});
+
+	// calculate the code and show the code in the user
+	return c.json({
+		code: otp,
+	});
+});
+
+// save the token to the database
+app.post('/api/token/save', async (c) => {
+	const { algorithm, digits, issuer, label, secret, period } = await c.req.json();
+
+	// encrypt the secret
+	const encryptedSecret = await encrypt({
+		data: secret,
+		keyB64: c.env.ENCRYPTION_KEY,
+	});
+
+	// check if the issuer already exist in the database, then update the token
+	const { results: issuerExists } = await c.env.DB.prepare(`SELECT * FROM Tokens WHERE Issuer = ?`).bind(issuer).all();
+	if (issuerExists.length > 0) {
+		const { result } = await c.env.DB.prepare(
+			`UPDATE Tokens SET TimeStep = ?, EncryptedSecret = ?, Algorithm = ?, Digits = ? WHERE Issuer = ?`,
+		)
+			.bind(period, encryptedSecret, algorithm, digits, issuer)
+			.all();
+		return c.json({
+			info: 'Issuer already exists, updated the token.',
+		});
+	}
+
+	// insert the token to the database
+	const { result } = await c.env.DB.prepare(
+		`INSERT INTO Tokens (Issuer, TimeStep, EncryptedSecret, Algorithm, Digits) VALUES (?, ?, ?, ?, ?)`,
+	)
+		.bind(issuer, period, encryptedSecret, algorithm, digits)
+		.all();
+	return c.json({
+		info: 'Token saved successfully.',
+	});
+});
 export default app;
