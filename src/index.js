ebbcc0cfb21cb1d3767e94cebbd1d7346afa8ff9
alanjames00 <alanjames6282@gmail.com>
2024-10-08 02:02:26 +0530

    fix: add authware for all endpoints

--- src/index.js
+++ src/index.js
@@ -12,11 +12,7 @@ const app = new Hono();
 // session authentication middleware
 
 const authWare = createMiddleware(async (c, next) => {
-	console.log(`[${c.req.method}] ${c.req.url}`);
-	console.log('checking for session');
-
 	const session = await getCookie(c, 'session');
-	console.log('session', session);
 
 	if (!session) {
 		return c.redirect('/login');
@@ -24,7 +20,6 @@ const authWare = createMiddleware(async (c, next) => {
 	const { results: serverSession } = await c.env.DB.prepare(`SELECT * FROM Sessions WHERE SessionId = ? AND UserEmail = ?`)
 		.bind(session, c.env.USER_EMAIL)
 		.all();
-	console.log('serverSession', serverSession);
 	if (serverSession.length === 0) {
 		// session not found in session store
 		return c.redirect('/login');
@@ -38,9 +33,38 @@ const authWare = createMiddleware(async (c, next) => {
 });
 
 app.use('/', authWare);
+app.use('/scan', authWare);
+app.use('/api/token/new', authWare);
+app.use('/api/token/save', authWare);
 
-app.get('/', (c) => {
-	return c.render(renderHomepage());
+app.get('/', async (c) => {
+	// get all the tokens from the database
+	const { results: tokens } = await c.env.DB.prepare(`SELECT * FROM Tokens`).all();
+
+	// iterate over the tokens and generate the totp
+	let results = [];
+	for (let token of tokens) {
+		const secret = await decrypt({
+			base64Combined: token.EncryptedSecret,
+			key: c.env.ENCRYPTION_KEY,
+		});
+
+		// generate totp
+		const otp = await generateTotp({
+			algorithm: token.Algorithm,
+			digits: token.Digits,
+			period: token.TimeStep,
+			secret,
+		});
+
+		results.push({
+			issuer: token.Issuer,
+			otp,
+			timeStep: token.TimeStep,
+		});
+	}
+
+	return c.render(renderHomepage({ tokens: results }));
 });
 
 app.get('/login', (c) => {
@@ -49,19 +73,15 @@ app.get('/login', (c) => {
 
 app.post('/login', async (c) => {
 	const { email, password } = await c.req.parseBody();
-	console.log('email', email);
-	console.log('password', password);
 	// authenticate user
 	const isMatched = email === c.env.USER_EMAIL && password === c.env.USER_PASSWORD;
 	// if successful, create a session
-	console.log('isMatched', isMatched);
 
 	if (isMatched) {
 		const session = await crypto.randomUUID();
 
 		// insert to sessions table
 		const { result } = await c.env.DB.prepare(`UPDATE Sessions SET SessionId = ? WHERE UserEmail = ?`).bind(session, email).all();
-		console.log('result of query', result);
 
 		await setCookie(c, 'session', session, { httpOnly: true, maxAge: 60 * 10 });
 		return c.redirect('/');
